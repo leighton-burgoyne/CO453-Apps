8d43e7c0fe5cbdbdffbbd49d82c2159430edc7b9
Leighton Burgoyne <91318744+leighton-burgoyne@users.noreply.github.com>
2022-02-06 17:39:11 +0000

    Added Menu for selecting programs to execute.

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -11,15 +11,57 @@ namespace ConsoleAppProject
     /// 
     /// This Project has been modified by:
     /// Derek Peacock 14/12/2020
+    /// Leighton Burgoyne 06/02/2022
     /// </summary>
     public static class Program
     {
         public static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            
-            Console.WriteLine("BNU CO453 Applications Programming 2021-2022!");
-            Console.WriteLine();
+
+            Console.WriteLine("BNU CO453 Applications Programming 2021-2022");
+            Console.WriteLine("Created by Leighton Burgoyne");
+            Menu();
+        }
+
+        public static void Menu() // Menu Navigation
+        {
+            Console.WriteLine("Please select a Program to Run:"); // User Prompt
+            Console.WriteLine("1. App01: Distance Converter"); // Distance Converter
+            Console.WriteLine("2. App02: BMI Calculator"); // BMI Calculator
+            Console.WriteLine("3. App03: Student Marks"); // Student Marks
+            Console.WriteLine("4. App04: Social Network"); // Social Network
+            Console.WriteLine("5. App05: RPS Game"); // RPS Game
+
+            string SelectedApp = Console.ReadLine(); // Read the User Input from the Console
+
+            switch (SelectedApp) // Switch and Case Method for Option Selection
+            {
+                case "1": // App01: Distance Converter
+                    Console.WriteLine("Loading App01: Distance Converter");
+                    // Run Function
+                    break;
+                case "2": // App02: BMI Calculator
+                    Console.WriteLine("Loading App02: BMI Calculator");
+                    // Run Function
+                    break;
+                case "3": // App03: Student Marks
+                    Console.WriteLine("Loading App03: Student Marks");
+                    // Run Function
+                    break;
+                case "4": // App04: Social Network
+                    Console.WriteLine("Loading App04: Social Network");
+                    // Run Function
+                    break;
+                case "5": // App05: RPS Game
+                    Console.WriteLine("Loading App05: RPS Game");
+                    // Run Function
+                    break;
+                default: // Invalid Input
+                    Console.WriteLine("Invalid Input: Please specify an option from the list above");
+                    // Run Function
+                    break;
+            }
         }
     }
 }
