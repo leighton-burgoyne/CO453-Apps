0098faa3ea9a4d330795f59353ab1e1030ce357e
Leighton Burgoyne <91318744+leighton-burgoyne@users.noreply.github.com>
2022-02-12 13:53:06 +0000

    Removed Privacy Page as not required

--- WebApps/Controllers/HomeController.cs
+++ WebApps/Controllers/HomeController.cs
@@ -31,11 +31,6 @@ namespace WebApps.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
