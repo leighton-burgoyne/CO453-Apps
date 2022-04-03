734e4df2fb1e760b6ed616b5e9d1a76f9c1ef8b9
Leighton Burgoyne <91318744+leighton-burgoyne@users.noreply.github.com>
2022-04-03 20:10:44 +0100

    Refactored & Added Inheritance and a Base Class

---
+++ ConsoleAppProject/App04/Post.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppProject.App04
+{
+    public class Post
+    {
+        private int likes;
+
+        private readonly List<string> comments;
+
+        // username of the post's author
+        public string Username { get; }
+        public DateTime Timestamp { get; }
+
+        public Post(string author)
+        {
+            this.Username = author;
+            Timestamp = DateTime.Now;
+
+            likes = 0;
+            comments = new List<string>();
+        }
+
+        /// <summary>
+        /// Record one more 'Like' indication from a user.
+        /// </summary>
+        public void Like()
+        {
+            likes++;
+        }
+
+        ///<summary>
+        /// Record that a user has withdrawn his/her 'Like' vote.
+        ///</summary>
+        public void Unlike()
+        {
+            if (likes > 0)
+            {
+                likes--;
+            }
+        }
+
+        ///<summary>
+        /// Add a comment to this post.
+        /// </summary>
+        /// <param name="text">
+        /// The new comment to add.
+        /// </param>        
+        public void AddComment(string text)
+        {
+            comments.Add(text);
+        }
+
+        ///<summary>
+        /// Display the details of this post.
+        /// 
+        /// (Currently: Print to the text terminal. This is simulating display 
+        /// in a web browser for now.)
+        ///</summary>
+        public void Display()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"    Author: {Username}");
+            Console.WriteLine($"    Time Elpased: {FormatElapsedTime(Timestamp)}");
+            Console.WriteLine();
+
+            if (likes > 0)
+            {
+                Console.WriteLine($"    Likes:  {likes}  people like this.");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            if (comments.Count == 0)
+            {
+                Console.WriteLine("    No comments.");
+            }
+            else
+            {
+                Console.WriteLine($"    {comments.Count}  comment(s). Click here to view.");
+            }
+        }
+
+        ///<summary>
+        /// Create a string describing a time point in the past in terms 
+        /// relative to current time, such as "30 seconds ago" or "7 minutes ago".
+        /// Currently, only seconds and minutes are used for the string.
+        /// </summary>
+        /// <param name="time">
+        ///  The time value to convert (in system milliseconds)
+        /// </param> 
+        /// <returns>
+        /// A relative time string for the given time
+        /// </returns>      
+        private string FormatElapsedTime(DateTime time)
+        {
+            DateTime current = DateTime.Now;
+            TimeSpan timePast = current - time;
+
+            long seconds = (long)timePast.TotalSeconds;
+            long minutes = seconds / 60;
+
+            if (minutes > 0)
+            {
+                return minutes + " minutes ago";
+            }
+            else
+            {
+                return seconds + " seconds ago";
+            }
+        }
+    }
+}
