5638a5dd4df1ba99250b2728048f20b311d0d2b4
Leighton Burgoyne <91318744+leighton-burgoyne@users.noreply.github.com>
2022-04-13 15:37:09 +0100

    Added ConsoleTests project for Unit Tests

---
+++ ConsoleTests/DistanceConverterUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleTests
+{
+    [TestClass]
+    public class DistanceConverterUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ConsoleTests/StudentGradesUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleTests
+{
+    [TestClass]
+    public class StudentGradesUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
