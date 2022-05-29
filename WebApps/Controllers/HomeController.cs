85670bbbec0fa062e91250030ff08d5a6f66f535
Leighton Burgoyne <91318744+leighton-burgoyne@users.noreply.github.com>
2022-05-29 12:04:38 +0100

    Completed most WebApps

--- WebApps/Controllers/HomeController.cs
+++ WebApps/Controllers/HomeController.cs
@@ -6,37 +6,71 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApps.Models;
+using ConsoleAppProject.App02;
 
 namespace WebApps.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index() // Index Page View
         {
             return View();
         }
 
-        public IActionResult DistanceConverter()
+        public IActionResult DistanceConverter() // Distance Converter Page View
         {
             return View();
         }
 
-        public IActionResult BMICalculator()
+        [HttpGet]
+        public IActionResult BMICalculator() // BMI Calculator Page View
         {
             return View();
         }
 
-        public IActionResult StudentMarks()
+        [HttpPost]
+        public IActionResult BMICalculator(BMICalculatorWeb calculator) // BMI Calculator with BMI Page View
+        {
+            if (calculator.Centimetres > 140)
+            {
+                calculator.CalculateMetric();
+            }
+            else if (calculator.Feet > 4 && calculator.Stones > 6)
+            {
+                calculator.CalculateImperial();
+            }
+            else
+            {
+                ViewBag.Error = "Values too small";
+                return View();
+            }
+
+            double bmiValue = calculator.Bmi;
+  
+            return RedirectToAction("HealthMessage", new { bmiValue }); 
+        }
+
+        public IActionResult HealthMessage(double bmiValue) // BMI Calculator Health Message Page View
+        {
+            return View(bmiValue);
+        }
+
+        public IActionResult StudentMarks() // Student Marks Page View
         {
             return View();
         }
-        public IActionResult SocialNetwork()
+        public IActionResult SocialNetwork() // Social Network Page View
+        {
+            return View();
+        }
+
+        public IActionResult MonoGame() // MonoGame Page View
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error() // Error Handling
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
---
+++ WebApps/Views/Home/BMICalculator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApps.Views.Home
+{
+    public class BMICalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
